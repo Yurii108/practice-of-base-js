5f381f61aa4874ea44d00f8e80f422cbdf83640e
paraska108@gmail.com <paraska108@gmail.com>
2022-03-24 19:42:07 +0200

    ASD

---
+++ script.js
@@ -0,0 +1,51 @@
+"use strict";
+
+
+// let number = 5;
+// const leftBorder = 1;
+
+// number = 10;
+// console.log(number);
+
+// let number = 5;
+// const leftBorder = 1;
+
+// number = 10;
+// console.log(number);
+
+// const obj = {
+//     name: 'Yurii',
+//     age: 27,
+//     isMerried: false
+// };
+
+// console.log(obj .age);
+
+// let arr = ['car.jpg', 'bycicle.png', 8,];
+// console.log(arr[1]);
+
+// const answer = prompt('Вам есть 18 лет?', '');
+// console.log(answer);
+
+// const answers = [];
+
+// answers[0] = prompt("Ваше имя?", "");
+// answers[1] = prompt("Ваше Фамилия?", "");
+// answers[2] = prompt("Сколько вам лет?", "");
+
+// document.write(answers);
+
+// const category = 'toys';
+
+// console.log(`https://youtube.com/${category}/8`);
+
+// console.log(4 + +'8');
+
+const isChecked = true,
+        isClose = false;
+
+console.log(isChecked || isClose);
+
+let namber = 10;
+
+let first = 20; 
