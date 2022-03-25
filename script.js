1deecb813a49b018fdedc24d4b070b8034876ce2
Yuriy_PS <paraska108@gmail.com>
2022-03-25 12:50:28 +0200

    Created project: namber0films

--- script.js
+++ script.js
@@ -1,51 +1,21 @@
 "use strict";
 
+const namber0films = +prompt('Сколько фильмов вы помотрели?', '');
 
-// let number = 5;
-// const leftBorder = 1;
+const personalMovieDV = {
+        count: namber0films,
+        movies: {},
+        actors: {},
+        genres: [],
+        pravet: false
+};
 
-// number = 10;
-// console.log(number);
+const a = prompt('Один из просмотренных ранее фильмов?', ''),
+      b = prompt('Насолько оцените его?', ''),
+      c = prompt('Один из просмотренных ранее фильмов?', ''),
+      d = prompt('Насолько оцените его?', '');
 
-// let number = 5;
-// const leftBorder = 1;
+personalMovieDV.movies[a] = b;
+personalMovieDV.movies[c] = d;
 
-// number = 10;
-// console.log(number);
-
-// const obj = {
-//     name: 'Yurii',
-//     age: 27,
-//     isMerried: false
-// };
-
-// console.log(obj .age);
-
-// let arr = ['car.jpg', 'bycicle.png', 8,];
-// console.log(arr[1]);
-
-// const answer = prompt('Вам есть 18 лет?', '');
-// console.log(answer);
-
-// const answers = [];
-
-// answers[0] = prompt("Ваше имя?", "");
-// answers[1] = prompt("Ваше Фамилия?", "");
-// answers[2] = prompt("Сколько вам лет?", "");
-
-// document.write(answers);
-
-// const category = 'toys';
-
-// console.log(`https://youtube.com/${category}/8`);
-
-// console.log(4 + +'8');
-
-const isChecked = true,
-        isClose = false;
-
-console.log(isChecked || isClose);
-
-let namber = 10;
-
-let first = 20; 
+console.log(personalMovieDV);
