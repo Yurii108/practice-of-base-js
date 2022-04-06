40ae18efbc62b757e378e97486231c0d00776d14
Yuriy_PS <paraska108@gmail.com>
2022-04-06 15:22:13 +0300

    Continue

--- TrainingPage.js
+++ TrainingPage.js
@@ -63,3 +63,81 @@
 // personalMovieDV.movies[c] = d;
         
 // console.log(personalMovieDV);
+
+// document.querySelector(".header").innerText = "Hi Yurii";
+
+// const options = {
+//     name: 'test',
+//     width: 1024,
+//     height: 1024,
+//     colors: {
+//         border: 'black',
+//         bg: 'red'
+//     }
+// };
+
+// // console.log(options.name);
+
+// // delete options.name;
+
+// // console.log(options);
+
+// for (let key in options) {
+//     if (typeof(options[key]) === 'object') {
+//         for (let i in options[key]) {
+//             console.log(`Свойства ${i} имеет значение ${options[key][i]}`);
+//         }
+//      } else {
+//             console.log(`Свойства ${key} имеет значение ${options[key]}`);
+//         }
+//     }
+
+// let arr = [1, 2, 3, 4, 8, 6, 7];
+
+// for (let i = 0; i < arr.length; i++) {
+// console.log(arr[i]);
+// }
+
+
+// let qvation = prompt('Як діла?');
+// console.log(qvation);
+
+// function func() {
+//     for (let i = 1; i <= 5; i++) {
+//         console.log(i); 
+//     }
+
+// }
+
+// func();
+
+// let actorss = ['masha', 'vasya', 'pety'];
+
+// function myActorss() {
+   
+//     for (let isd of actorss) {
+//         console.log(isd);
+//     }
+//     for (let i = 0; i < actorss.length; i++) {
+//         console.log(actorss[i]);  
+//     }
+// }
+
+// myActorss();
+
+// function nunu(n1, n2){
+//     return (n1 * n2); 
+// }
+// console.log(nunu(45, 4));
+
+
+
+let countSheep = function (num){
+    let sheeps = '';
+    for (let i = 1; i <= num; i++) {
+    sheeps += `${i} sheep... `;
+    }
+    return sheeps;
+    };
+
+    console.log(countSheep(10));
